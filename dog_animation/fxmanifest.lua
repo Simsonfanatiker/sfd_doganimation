ee588cbac1548a04a9af9c09383f3f98f8ab61f6
Simsonfanatiker <74657073+Simsonfanatiker@users.noreply.github.com>
2022-06-28 20:53:40 +0200

    Publish on GitHub
    Version 2.0

---
+++ dog_animation/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Simsonfanatiker Development'
+description 'Simple Dog Animation Menu'
+version '2.0.0'
+
+client_scripts {
+	'@warmenu_neu/warmenu.lua',
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
